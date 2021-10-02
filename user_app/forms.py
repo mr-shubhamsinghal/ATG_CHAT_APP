49e357159594bc474f1ae5a0962947e59c1960ff
mr-shubhamsinghal <singhal121997@gmail.com>
2021-10-02 12:27:52 +0530

    implement captcha

--- user_app/forms.py
+++ user_app/forms.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# from math_captcha.forms import MathCaptchaForm
 
 class UserRegisterForm(UserCreationForm):
 
